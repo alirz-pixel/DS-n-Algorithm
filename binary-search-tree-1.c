1e5295ef2883e6606db60806d3d8d0378bc4b420
alirz-pixel <alirce@naver.com>
2021-04-30 18:06:43 +0900

    Add deleteLeafNode function

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -241,6 +241,78 @@ int deleteLeafNode(Node* head, int key)
 		printf("Error! : initializeBST가 제대로 수행되었는지 확인해 주세요.\n");
 		return -1;
 	}
+
+	// 트리에 아무 데이터도 없을 경우
+	if (head->left == NULL)
+	{
+		printf("Error! : 트리에 아무 데이터도 없습니다.\n");
+		return -1;
+	}
+
+
+	Node *previous = head;
+	Node *searchNode = head->left; 
+
+	// searchNode가 left node 일 때까지 반복
+	while (searchNode->left != NULL || searchNode->right != NULL)
+	{
+		previous = searchNode;
+
+		// 현재 노드의 key값과 입력받은 key값이 같은 경우, 반복문 종료
+		if (searchNode->key == key) break;
+
+		// searchNode의 key 값보다 입력받은 key 값이 큰 경우
+		if (searchNode->key < key)
+		{
+			// 만약, serahcNode의 오른쪽 자식 노드가 없는 경우
+			if (searchNode->right == NULL)
+			{
+				printf("삭제하고자 하는 노드가 트리에 없습니다.\n");
+				return -1;
+			}
+			
+			// 아닐 경우, searchNode의 오른쪽 서브 트리 탐색
+			searchNode = searchNode->right;
+		}
+
+		// searchNode의 key값이 입력받은 key값보다 작은 경우
+		else 
+		{
+			// 만약, serahcNode의 왼쪽 자식 노드가 없는 경우
+			if (searchNode->left == NULL)
+			{
+				printf("삭제하고자 하는 노드가 트리에 없습니다.\n");
+				return -1;
+			}
+
+			// 아닐 경우, seasrchNode의 왼쪽 서브 트리 탐색
+			searchNode = searchNode->left;
+		}
+	}
+
+	// 입력받은 key값과 동일한 key값을 가지는 노드는 찾았으나, leaf node가 아닌 경우
+	if (searchNode->left != NULL || searchNode->right != NULL)
+	{
+		printf("the node [%d] is not a leaf\n", searchNode->key);
+		return -1;
+	}
+
+	
+	free(searchNode);
+
+	// 루트 노드일 때
+	if (previous == head)
+		head->left = NULL;
+	
+	// 오른쪽 자식 노드의 메모리를 해제한 후, 이 부모 노드의 right를 NULL로 만들어준다.
+	else if (previous->key < key)
+		previous->right = NULL;
+
+	// 왼쪽 자식 노드의 메모리를 해제한 후, 이 부모 노드의 left를 NULL로 만들어준다.
+	else
+		previous->left = NULL;
+
+	return 1;
 }
 
 Node* searchRecursive(Node* ptr, int key)
